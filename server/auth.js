5b40626e4266d26cde0b610d76cdf53796d08a24
Andrew Williams <retiredpirate42+gitkraken@gmail.com>
2020-11-01 14:31:08 -0800

    Deleting Low DB
    And correctly formatting JWT

--- server/auth.js
+++ server/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-const { models } = require("./mdb")
+const { models } = require("./db")
 const secret = "catpack"
 const { AuthenticationError, ForbiddenError } = require("apollo-server")
 
@@ -8,7 +8,7 @@ const { AuthenticationError, ForbiddenError } = require("apollo-server")
  * using user.id and user.role
  * @param {Object} user the user to create a jwt for
  */
-const createToken = id => jwt.sign(id, secret)
+const createToken = id => jwt.sign({ id }, secret)
 
 /**
  * will attemp to verify a jwt and find a user in the
@@ -18,7 +18,7 @@ const createToken = id => jwt.sign(id, secret)
  */
 const getUserFromToken = async token => {
   try {
-    const id = jwt.verify(token, secret)
+    const { id } = jwt.verify(token, secret)
     const user = await models.User.findById(id)
     return user
   } catch (e) {
